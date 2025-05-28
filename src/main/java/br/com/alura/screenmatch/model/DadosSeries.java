887948c750e7b14473c73d8aefbf11b0d6285818
Taykazuhiro <taykazuhiro@gmail.com>
2025-05-28 15:48:53 -0300

    novos itens adicionados a record series

--- src/main/java/br/com/alura/screenmatch/model/DadosSeries.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSeries.java
@@ -3,6 +3,8 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 /* @JsonAlias e @JsonProperty são notações da dependencia Jackson para serialização e deserialização
 JsonAlias é usado apenas apenas na desearilização (leitura). Posso colocar um array de nomes para busca.
 JsonProperty é uma para serialização e desearilização usando a mesma chave que ele buscou.
@@ -10,5 +12,10 @@ JsonProperty é uma para serialização e desearilização usando a mesma chave 
 @JsonIgnoreProperties (ignoreUnknown = true)
 public record DadosSeries(@JsonAlias("Title") String titulo,
                           @JsonAlias("totalSeasons") Integer totalTemporadas,
-                          @JsonAlias("imdbRating") String avaliacao) {
+                          @JsonAlias("imdbRating") String avaliacao,
+                          @JsonAlias("Genre") String categoria,
+                          @JsonAlias("Poster") String imagem,
+                          @JsonAlias("Plot") String sinopse,
+                          @JsonAlias("Actors") String atores)
+{
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -14,38 +14,52 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=e91e2a6b";
     private ConverteDados conversor = new ConverteDados();
+    private List<DadosSeries> dadosSeries = new ArrayList<>();
 
     public void exibeMenu() {
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                
-                0 - Sair                                 
-                """;
+        var opcao = -1;
+        while (opcao != 0){
+    var menu = """
+            1 - Buscar séries
+            2 - Buscar episódios
+            3 - Listar séries buscadas
+            
+            0 - Sair                                 
+            """;
 
         System.out.println(menu);
-        var opcao = leitura.nextInt();
+        opcao =leitura.nextInt();
         leitura.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
-        }
+        switch(opcao)
+
+    {
+        case 1:
+            buscarSerieWeb();
+            break;
+        case 2:
+            buscarEpisodioPorSerie();
+            break;
+        case 3:
+            listarSeriesBuscadas();
+            break;
+        case 0:
+            System.out.println("Saindo...");
+            break;
+        default:
+            System.out.println("Opção inválida");
+    }
+}
+    }
+
+    private void listarSeriesBuscadas() {
+        dadosSeries.forEach(System.out::println);
     }
 
 
     private void buscarSerieWeb() {
         DadosSeries dados = getDadosSerie();
-        System.out.println(dados);
+        dadosSeries.add(dados);
     }
 
     private DadosSeries getDadosSerie() {
